9d20095bc3f9e5407e24705f4e2348d744541433
Zephyrrus <krisztian1997@gmail.com>
2020-04-02 02:48:41 +0300

    Remove useless services and add traefik

--- hayden/entrypoint.sh
+++ hayden/entrypoint.sh
@@ -8,30 +8,10 @@ else
   export RUNEXEC=gosu
 fi
 
+# COPY config.conf config.conf
+
 USER_ID=${UID}
 GROUP_ID=${GID}
-
-export SCRAPER_BOARDS=$(echo $SCRAPER_BOARDS | sed "s/^/\"/;s/$/\"/;s/\s*,\s*/\",\"/g")
-
-if [ -z "$SCRAPER_PROXY" ]; then
-    export SCRAPER_PROXY_CONF=""
-else
-    export SCRAPER_PROXY_CONF=$( envsubst <<\EOF
-  "proxies" : [
-      {
-          "type" : "${SCRAPER_PROXY_TYPE}",
-          "url" : "${SCRAPER_PROXY_URL}",
-          "username" : "${SCRAPER_PROXY_USER}",
-          "password" : "${SCRAPER_PROXY_PASS}"
-      }
-  ],
-EOF
-)
-fi
-
-if [ ! -f "/hayden/config.json" ]; then
-  envsubst < /hayden/.config.json.env > /hayden/config.json
-fi
 
 echo "Setting permissions to UID/GID: ${USER_ID}/${GROUP_ID}"
 chown ${USER_ID}:${GROUP_ID} -R /hayden
